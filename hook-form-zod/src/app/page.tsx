d20ed1896d8a1e1c19271f66ca6fe91bfa2f9a4c
pnauj <torres.jpw@gmail.com>
2024-06-08 20:41:14 -0300

    se maqueto el form para empezar a probar react-hook-form

--- hook-form-zod/src/app/page.tsx
+++ hook-form-zod/src/app/page.tsx
@@ -1,7 +1,39 @@
+"use client";
+import { useForm } from "react-hook-form";
+
 function HomePage() {
+  const { register, handleSubmit } = useForm();
+
   return (
-    <div>HomePage</div>
-  )
+    <div className="contenedor">
+      <form>
+        <label htmlFor="name">Name</label>
+        <input type="text" id="name" name="name" />
+
+        <label htmlFor="email">Email</label>
+        <input type="email" id="email" name="email" />
+
+        <label htmlFor="password">Password</label>
+        <input type="password" id="password" name="password" />
+
+        <label htmlFor="confirmPass">Confirm Password</label>
+        <input type="password" id="confirmPass" name="confirmPass" />
+
+        <label htmlFor="weight">Weight</label>
+        <input type="number" id="weight" name="weight" />
+
+        <label htmlFor="plan">Plan</label>
+        <select name="plan" id="plan">
+          <option value="free">Free</option>
+          <option value="basic">Basic</option>
+          <option value="medium">Medium</option>
+          <option value="prime">Prime</option>
+        </select>
+
+        <button type="submit">Submit</button>
+      </form>
+    </div>
+  );
 }
 
-export default HomePage
+export default HomePage;
